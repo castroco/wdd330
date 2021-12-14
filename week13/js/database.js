2bd7d4758128d2cd597dbb75094b371b1521deea
castroco <carlos.castro.p@gmail.com>
2021-12-14 10:10:45 -0300

    commit

--- week13/js/database.js
+++ week13/js/database.js
@@ -67,8 +67,143 @@ const baseCourse = {
     ]
 }
 
+const courses = [
+    {
+        courseName : 'Math13',
+        courseLongName : 'Math for Chilean High School 3rd Grade',
+        languaje : 'Spanish',
+        grade : "3rd High School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'English15',
+        courseLongName : 'English for Basic Level Speakers',
+        languaje : 'English',
+        grade : "3rd High School",
+        profile : 'Various teachers',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math12',
+        courseLongName : 'Math for Chilean High School 2nd Grade',
+        languaje : 'Spanish',
+        grade : "2nd High School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math11',
+        courseLongName : 'Math for Chilean High School 1st Grade',
+        languaje : 'Spanish',
+        grade : "1st High School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math14',
+        courseLongName : 'Math for Chilean High School 4th Grade',
+        languaje : 'Spanish',
+        grade : "4th High School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math08',
+        courseLongName : 'Math for Chilean Primary School 8th Grade',
+        languaje : 'Spanish',
+        grade : "8th Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math07',
+        courseLongName : 'Math for Chilean Primary School 7th Grade',
+        languaje : 'Spanish',
+        grade : "7th Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math06',
+        courseLongName : 'Math for Chilean Primary School 6th Grade',
+        languaje : 'Spanish',
+        grade : "6th Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math05',
+        courseLongName : 'Math for Chilean Primary School 5th Grade',
+        languaje : 'Spanish',
+        grade : "5th Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math04',
+        courseLongName : 'Math for Chilean Primary School 4th Grade',
+        languaje : 'Spanish',
+        grade : "4th Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math03',
+        courseLongName : 'Math for Chilean Primary School 3rd Grade',
+        languaje : 'Spanish',
+        grade : "3rd Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math02',
+        courseLongName : 'Math for Chilean Primary School 2nd Grade',
+        languaje : 'Spanish',
+        grade : "2nd Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    },
+    {
+        courseName : 'Math01',
+        courseLongName : 'Math for Chilean Primary School 1st Grade',
+        languaje : 'Spanish',
+        grade : "1st Primary School",
+        profile : 'Profe Alex',
+        modules :   [
+                    ]
+    }
+]
+
 export function get_courseDetail(course = null, profile = 'default') {
     let answer = baseCourse;
     //console.log("answer: ", answer);
     return answer;
+}
+export function get_courses(courseGrade) {
+    console.log("courseGrade: ", courseGrade);
+    if (courseGrade == 'All') {
+        return courses;
+    }
+    else {
+        let answer = [];
+        courses.forEach(course => {
+            if (course.grade == courseGrade) {
+                answer.push(course);
+            }
+        });
+        return answer; 
+    }
 }
--- week13/js/main.js
+++ week13/js/main.js
@@ -1,6 +1,9 @@
-import { get_courseDetail } from './database.js';
+import { get_courseDetail, get_courses } from './database.js';
 import { displayCourse } from './display.js';
 
 let data = get_courseDetail();
 //console.log("data: ", data);
 displayCourse(data,'#nav1');
+let courses = get_courses('3rd High School');
+console.log("courses: ", courses);
+
